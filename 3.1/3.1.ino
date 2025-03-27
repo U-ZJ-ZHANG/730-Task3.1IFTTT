01a270d8291a738492659fc65531df9bfd0df48b
U-ZJ-ZHANG <s219015758@deakin.edu.au>
2025-03-27 11:40:14 +1100

    Update 3.1.ino

--- 3.1/3.1.ino
+++ 3.1/3.1.ino
@@ -1,17 +1,22 @@
 #include <WiFiNINA.h>
 #include <ArduinoHttpClient.h>
 
+// WiFi credentials
 char ssid[] = "2.4G";
 char pass[] = "WASD3.1415926";
 
+// IFTTT Webhooks key (from your IFTTT account)
 String key = "blQRbxPzlbKNssDhYfV2oM";
 
+// Sensor configuration
 const int lightSensorPin = A0;
-int threshold = 500; 
-bool light = false; 
+int threshold = 500;
+bool light = false;
 
-char server[] = "maker.ifttt.com"; 
+// IFTTT server address
+char server[] = "maker.ifttt.com";
 
+// Create WiFi and HTTP client instances
 WiFiClient wifi;
 HttpClient client = HttpClient(wifi, server, 80);
 
@@ -28,22 +33,26 @@ void setup() {
 }
 
 void loop() {
+  // Read analog value from the light sensor
   int lightValue = analogRead(lightSensorPin);
   Serial.print("Light Level: ");
   Serial.println(lightValue);
 
+  // Detect if light appears and hasn't already been detected
   if (!light && lightValue > threshold) {
     light = true;
-    sendIFTTT("light_detected");
+    sendIFTTT("light_detected"); // Send event to IFTTT when light is detected
   }
+  // Detect if light disappears after being detected
   else if (light && lightValue < threshold - 100) {
     light = false;
     sendIFTTT("light_end");
   }
 
-  delay(5000);  
+  delay(5000);
 }
 
+// Function to send an event to IFTTT using Webhooks
 void sendIFTTT(String eventName) {
   String url = "/trigger/" + eventName + "/with/key/" + key;
   client.get(url);
